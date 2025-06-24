afc1cd2048ad6a38a852383eeeb94964ce392ff7
Rahat <rahatakhter4@gmail.com>
2025-06-24 17:53:38 +0600

    WIP: GlobalExceptionHandler

---
+++ cards/src/main/java/com/eazybytes/cards/controller/CardsController.java
@@ -0,0 +1,21 @@
+package com.eazybytes.cards.controller;
+
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(
+        name = "CRUD REST APIs for Cards in EazyBank",
+        description = "CRUD REST APIs in EazyBank to CREATE, UPDATE, FETCH AND DELETE card details"
+)
+@RestController
+@RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
+@AllArgsConstructor
+@Validated
+public class CardsController {
+    
+}
---
+++ cards/src/main/java/com/eazybytes/cards/service/impl/CardsServiceImpl.java
@@ -0,0 +1,43 @@
+package com.eazybytes.cards.service.impl;
+
+
+import com.eazybytes.cards.dto.CardsDto;
+import com.eazybytes.cards.entity.Cards;
+import com.eazybytes.cards.exception.CardAlreadyExistsException;
+import com.eazybytes.cards.repository.CardsRepository;
+import com.eazybytes.cards.service.ICardsService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class CardsServiceImpl implements ICardsService {
+
+    private CardsRepository cardsRepository;
+
+    @Override
+    public void createCard(String mobileNumber) {
+        Optional<Cards> optionalCards= cardsRepository.findByMobileNumber(mobileNumber);
+        if(optionalCards.isPresent()){
+            throw new CardAlreadyExistsException("Card already registered with given mobileNumber "+mobileNumber);
+        }
+        cardsRepository.save(createNewCard(mobileNumber));
+    }
+
+    @Override
+    public CardsDto fetchCard(String mobileNumber) {
+        return null;
+    }
+
+    @Override
+    public boolean updateCard(CardsDto cardsDto) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteCard(String mobileNumber) {
+        return false;
+    }
+}
