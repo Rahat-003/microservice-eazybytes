dcf244386da89ab35f6bbf45f1f81189c45d412b
Akhtar Rahat <rahatakhter4@gmail.com>
2025-06-25 21:47:16 +0600

    fixed AuditAwareImpl. Getting bean named 'auditAwareImpl' that could not be found

--- loans/src/main/java/com/eazybytes/loans/audit/AuditAwareImpl.java
+++ loans/src/main/java/com/eazybytes/loans/audit/AuditAwareImpl.java
@@ -1,12 +1,12 @@
 package com.eazybytes.loans.audit;
 
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
-@ComponentScan("auditAwareImpl")
+@Component("auditAwareImpl")
 public class AuditAwareImpl implements AuditorAware<String> {
     /**
      * Returns the current auditor of the application.
