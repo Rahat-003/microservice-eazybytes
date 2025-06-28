bad787a59b320be9e6f30ab26048b196a7d6169f
Akhtar Rahat <rahatakhter4@gmail.com>
2025-06-29 03:38:58 +0600

    added new commands

--- docker-commands.sh
+++ docker-commands.sh
@@ -2,6 +2,20 @@
 #Give the following commands inside each service directory. (accounts, cards, loans)
 # Microservices/microservice-eazybytes/accounts
 
+
+# To sync the docker desktop with the terminal's docker context.
+# Before running this command, Get help from internet
+docker context show
+
+unset DOCKER_HOST
+
+docker context use desktop-linux
+
+docker context show
+
+
+
+
 #Show all images
 docker images
 
@@ -25,5 +39,32 @@ docker stop <container_id>
 # To remove a container
 docker rm <container_id>
 
+# after adding following in pom.xml of loans service run the following command
+#  <image>
+#			<name>rahat1722/${project.artifactId}:section4</name>
+#  </image>
+mvn spring-boot:build-image
 
-Last checked: 45-Running accounts microservice as a Docker container.mp4
+
+# To create image using jib, search internet for jib-maven-plugin for cards service.
+# Using jib, docker image can be directly uploaded to remote repository like docker hub, google cloud or amazon elastic container registry.
+# Can use CI/CD pipeline to upload image to remote repository.
+
+#<plugin>
+#  <groupId>com.google.cloud.tools</groupId>
+#  <artifactId>jib-maven-plugin</artifactId>
+#  <version>3.3.2</version>
+#  <configuration>
+#      <to>
+#        <image>rahat1722/${project.artifactId}:section4</image>
+#      </to>
+#  </configuration>
+#</plugin>
+mvn compile jib:dockerBuild
+
+
+
+# Use buildpacks for creating images. Best option so far.
+
+
+Last checked video: 50
