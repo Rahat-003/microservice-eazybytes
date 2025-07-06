44c5f3bd68c10e297d4ef28c701096b62ca7dcb2
Akhtar Rahat <rahatakhter4@gmail.com>
2025-07-07 03:02:34 +0600

    WIP: Refresh configurations using refresh actuator path

--- loans/src/main/java/com/eazybytes/loans/dto/LoansContactInfoDto.java
+++ loans/src/main/java/com/eazybytes/loans/dto/LoansContactInfoDto.java
@@ -1,6 +1,8 @@
 package com.eazybytes.loans.dto;
 
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
@@ -8,5 +10,11 @@ import java.util.Map;
 
 
 @ConfigurationProperties(prefix = "loans")
-public record LoansContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Getter
+@Setter
+public class LoansContactInfoDto {
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
+
